4d66f5d5ef98337d031e3d4eaa4809a92c407b1f
dmccormack <dmccormack@netsuite.com>
2017-01-12 10:52:39 -0600

    Init project

---
+++ src/client/test_harness_client.ts
@@ -0,0 +1,10 @@
+/**
+ * @NAPIVersion 2.0
+ * @NScriptType ClientScript
+ */
+
+import {EntryPoints} from "N/types";
+
+export function pageInit(context: EntryPoints.Client.pageInitContext) {
+    alert("Client script working");
+};
---
+++ src/suitelet/test_harness.ts
@@ -0,0 +1,14 @@
+/**
+ * @NAPIVersion 2.0
+ * @NScriptType Suitelet
+ */
+
+import {EntryPoints} from "N/types";
+import * as ui from "N/ui/serverWidget";
+
+export function onRequest(context: EntryPoints.Suitelet.onRequestContext) {
+    if (context.request.method === "GET") {
+        // build form
+        // return form 
+    }
+}
